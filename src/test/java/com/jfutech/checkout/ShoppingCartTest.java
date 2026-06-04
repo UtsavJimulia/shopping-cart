b5db6f37ae6ab3a961e76a9d4cb14629859ef12c
Utsav Jimulia <utsavjimulia@Mac.lan>
2026-06-04 16:28:46 +0100

    step-1: failing test for empty cart

--- src/test/java/com/jfutech/checkout/ShoppingCartTest.java
+++ src/test/java/com/jfutech/checkout/ShoppingCartTest.java
@@ -1,6 +1,11 @@
 package com.jfutech.checkout;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ShoppingCartTest {
     private ShoppingCart shoppingCart;
@@ -8,5 +13,10 @@ public class ShoppingCartTest {
     @BeforeEach
     public void setUp() {
         shoppingCart = new ShoppingCart();
+    }
+
+    @Test
+    void shouldReturnZeroForEmptyCart() {
+        assertEquals("£0.00", shoppingCart.calculatePrice(List.of()));
     }
 }
